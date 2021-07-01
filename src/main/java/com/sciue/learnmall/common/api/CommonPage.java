1d50ef64be98a7762d01ba940318d945d815c8b3
sciue <1846416784@qq.com>
2021-07-01 10:40:26 +0800

    工具类添加

---
+++ src/main/java/com/sciue/learnmall/common/api/CommonPage.java
@@ -0,0 +1,75 @@
+package com.sciue.learnmall.common.api;
+
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+/**
+ * 分页信息封装类
+ * @param <T>
+ */
+public class CommonPage<T> {
+    private Integer pageNum;
+    private Integer pageSize;
+    private Integer totalPage;
+    private Long total;
+    private List<T> list;
+
+    /**
+     *
+     * @param list
+     * @param <T>
+     * @return
+     * 将PageHelper分页后的List封装成包含分页信息的对象
+     */
+    public static <T> CommonPage<T> restPage(List<T> list){
+        CommonPage<T> result = new CommonPage<T>();
+        PageInfo<T> pageInfo = new PageInfo<T>();
+        result.setTotalPage(pageInfo.getPages());
+        result.setPageNum(pageInfo.getPageNum());
+        result.setPageSize(pageInfo.getPageSize());
+        result.setTotal(pageInfo.getTotal());
+        result.setList(pageInfo.getList());
+        return result;
+    }
+
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Integer getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(Integer totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+}
